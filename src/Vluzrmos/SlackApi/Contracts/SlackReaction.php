17dd860f27a5f76d141dffd5dd1acfe51eeea5a8
Jeroen Coupe <>
2023-04-17 17:16:40 +0200

    no message

---
+++ src/Vluzrmos/SlackApi/Contracts/SlackReaction.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Vluzrmos\SlackApi\Contracts;
+
+interface SlackFile
+{
+    public function get($options = []);
+}
---
+++ src/Vluzrmos/SlackApi/Methods/Reaction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Vluzrmos\SlackApi\Methods;
+
+use Vluzrmos\SlackApi\Contracts\SlackReaction;
+
+class Reaction extends SlackMethod implements SlackReaction
+{
+    protected $methodsGroup = 'reactions.';
+
+
+    /**
+     * This method returns the reaction on a file or file comment.
+     *
+     * @param array $options ['file' = 'F1234567890', 'file_comment' => 'Fc1234567890', 'full'=>true, 'timestamp'=>'1524523204.000192']
+     */
+
+    public function get($options = [])
+    {
+        $this->method('get', $options);
+    }
+
+
+}
